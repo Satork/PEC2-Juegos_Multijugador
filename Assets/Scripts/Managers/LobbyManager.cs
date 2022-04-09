41a8d50dc2ae0bde831ca2399bbd6a0f89285320
SatorGod <jmarrero3499@gmail.com>
2022-04-09 16:11:58 +0100

    Lobby Update
     - Added Server choice on Join Button pressed

--- Assets/Scripts/Managers/LobbyManager.cs
+++ Assets/Scripts/Managers/LobbyManager.cs
@@ -1,22 +1,35 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using Mirror;
 using Mirror.Discovery;
+using TMPro;
+using UnityEditor.UIElements;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 namespace Managers
 {
     public class LobbyManager : MonoBehaviour
     {
-        readonly Dictionary<long, ServerResponse> discoveredServers = new Dictionary<long, ServerResponse>();
+	    private readonly Dictionary<long, ServerResponse> discoveredServers = new Dictionary<long, ServerResponse>();
         Vector2 scrollViewPos = Vector2.zero;
 
         public NetworkDiscovery networkDiscovery;
+        public GameObject m_MenuLayout;
+        public GameObject m_JoinLayout;
+        public GameObject m_ScrollContent;
 
-        
-        public GameObject servidores;
-        public RectTransform ParentPanel;
+        public GameObject m_ServerPrefab;
+
+        private List<GameObject> instances = new List<GameObject>();
+
+        private void Start() {
+	        m_MenuLayout.SetActive(true);
+	        m_JoinLayout.SetActive(false);
+        }
 
         public void NuevoServidor(){
             discoveredServers.Clear();
@@ -32,17 +45,45 @@ namespace Managers
         public void UnirsePartida(){
             discoveredServers.Clear();
             networkDiscovery.StartDiscovery();
+            m_MenuLayout.SetActive(false);
+            m_JoinLayout.SetActive(true);
+        }
 
+        public void Return() {
+	        instances.ForEach(Destroy);
+	        instances.Clear();
+	        discoveredServers.Clear();
+	        m_MenuLayout.SetActive(true);
+	        m_JoinLayout.SetActive(false);
         }
         /* TODO: falta que se muestre el listado de los servidores disponibles */
-        void Update(){
-            foreach (ServerResponse info in discoveredServers.Values)
-                Connect(info);                
+
+        private IEnumerator UpdateServers() {
+	        var posY = 25;
+	        if (discoveredServers.Count == instances.Count) yield break;
+	        Debug.Log($"UpdateServers: {discoveredServers.Count}");
+	        foreach (var server in discoveredServers) {
+		        var serverInstance = Instantiate(m_ServerPrefab, m_ScrollContent.transform);
+		        var serverButton = serverInstance.GetComponent<Button>();
+		        var serverText = serverInstance.GetComponent<TextMeshProUGUI>();
+
+		        serverInstance.transform.position += Vector3.down * posY;
+		        posY += posY;
+
+		        serverText.text = $"ServerID: {server.Value.uri}";
+
+		        serverButton.onClick.AddListener(delegate { Connect(server.Value); });
+
+		        instances.Add(serverInstance);
+	        }
+	        yield return null;
         }
-        
+
         public void OnDiscoveredServer(ServerResponse info)
         {
             discoveredServers[info.serverId] = info;
+            Debug.Log($"OnDiscoveredServer: {discoveredServers.Count}");
+            StartCoroutine(UpdateServers());
 
         }
         void Connect(ServerResponse info)
